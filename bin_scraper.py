ae2d13e38a29691c4e2b7f0cebd3e762c7ef6fc2
avesomo <radoslaw.rygiel@gmail.com>
2018-10-19 00:11:09 +0200

    added kline class, new functions, cleaning up

--- bin_scraper.py
+++ bin_scraper.py
@@ -1,54 +1,37 @@
-from help_functions import time_from_ts, get_coins_list, client
+from help_functions import time_from_ts, get_coins_list, client, Kline, print_ichimoku
 from binance.client import Client
 import matplotlib.pyplot as plt
 
 
-'''KLINES explained below
-[
-  [
-    1499040000000,      // Open time
-    "0.01634790",       // Open
-    "0.80000000",       // High
-    "0.01575800",       // Low
-    "0.01577100",       // Close
-    "148976.11427815",  // Volume
-    1499644799999,      // Close time
-    "2434.19055334",    // Quote asset volume
-    308,                // Number of trades
-    "1756.87402397",    // Taker buy base asset volume
-    "28.46694368",      // Taker buy quote asset volume
-    "17928899.62484339" // Ignore.
-  ]
-]'''
-
-
-def get_klines(markets, time_frame=Client.KLINE_INTERVAL_4HOUR):
+# todo - simplify time_frame inputs
+def get_klines(markets, time_frame=Client.KLINE_INTERVAL_4HOUR, time_interval='1 day ago UTC'):
     closing = 0
     for market in markets:
-        fig, vax = plt.subplots(1, 1, figsize=(10, 10))
+        all_klines = []
+        fig, vax = plt.subplots(1, 1, figsize=(30, 10))
         # todo - implement start_str into the variables
         for kline in client.get_historical_klines_generator(symbol=market,
                                                             interval=time_frame,
-                                                            start_str='1 day ago UTC'):
-            print(market, kline)
-            time, open_price, high_price, low_price, close_price, volume = tuple([float(i) for i in kline[:6]])
-            if closing < close_price:
+                                                            start_str=time_interval):
+
+            wick = Kline(*kline)
+            all_klines.append(wick)
+            print(market, wick.open_price)
+            # todo-print bars function
+            if closing < wick.close_price:
                 color = 'g'
             else:
                 color = 'r'
-            vax.vlines(time_from_ts(time), low_price, high_price, colors=color, lw=1)
-            vax.vlines(time_from_ts(time), open_price, close_price, colors=color, lw=10)
+            vax.vlines(wick.open_t, wick.low_price, wick.high_price, colors=color, lw=1)
+            vax.vlines(wick.open_t, wick.open_price, wick.close_price, colors=color, lw=10)
             # todo need to normalize the prices and volume to match scales
             # vax.vlines(time_from_ts(time), 0 , volume/100000, colors=color, lw=10)
-            closing = close_price
-        vax.set_xlabel('time')
-        vax.set_title(f'Vertical lines demo {market}')
-        # todo - rescale x-axis labels
-        plt.show()
+            closing = wick.close_price
+        print_ichimoku(all_klines, market, vax)
 
 
 # sample input
 markets = get_coins_list()[3:4]
-get_klines(markets, Client.KLINE_INTERVAL_30MINUTE)
+get_klines(markets, Client.KLINE_INTERVAL_4HOUR, '3 month ago UTC')
 st_ts = client.get_server_time()['serverTime']
 print(time_from_ts(st_ts))
--- help_functions.py
+++ help_functions.py
@@ -1,9 +1,27 @@
 import datetime
 from binance.client import Client
 from config import API_KEY, API_SECRET
-
+import matplotlib.pyplot as plt
+import numpy as np
 
 client = Client(API_KEY, API_SECRET)
+
+
+class Kline:
+    def __init__(self, open_time, open_price, high_price, low_price, close_price, volume,
+                 close_time, quote, no_trades, taker_base, taker_quote, ignore_val):
+        self.low_price = float(low_price)
+        self.close_price = float(close_price)
+        self.volume = float(volume)
+        self.close_time = int(close_time)
+        self.quote = float(quote)
+        self.no_trades = int(no_trades)
+        self.taker_base = float(taker_base)
+        self.taker_quote = float(taker_quote)
+        self.ignore_val = int(ignore_val)
+        self.high_price = float(high_price)
+        self.open_t = int(open_time)
+        self.open_price = float(open_price)
 
 
 def time_from_ts(timestamp, timespec='minutes'):
@@ -15,3 +33,68 @@ def time_from_ts(timestamp, timespec='minutes'):
 def get_coins_list():
     coins = [i['symbol'] for i in client.get_exchange_info()['symbols'] if 'BTC' in i['symbol']]
     return coins
+
+
+def print_ichimoku(klines, market, axis, conv_period=20, base_period=60, span2period=120, displacement=30):
+    # todo - rescale X-axis and redo subplots - they are misleading atm
+    # start = klines[0].open_t
+    # end = klines[-1].open_t
+    time = get_time(klines)
+    conversion_line = ((get_period_highs(conv_period, klines)
+                       + get_period_lows(conv_period, klines))
+                       / 2)
+    base_line = ((get_period_highs(base_period, klines)
+                 + get_period_lows(base_period, klines))
+                 / 2)
+    senkou_a = (conversion_line[-len(base_line):] + base_line)/2
+    senkou_b = ((get_period_highs(span2period, klines)
+                + get_period_lows(span2period, klines))
+                / 2)
+    chikou = get_chikou(displacement, klines)
+    plt.plot(time[:len(chikou)], chikou, '-', linewidth=0.5, markersize=1, color='g')
+    plt.plot(time[-len(conversion_line):], conversion_line, '-', linewidth=0.5, markersize=1, color='b')
+    plt.plot(time[-len(base_line):], base_line, '-', linewidth=0.5, markersize=1, color='r')
+
+    is_greater = senkou_a[-len(senkou_b):] > senkou_b
+    green_a = senkou_a[-len(senkou_b):]
+    green_b = senkou_b
+    time_a = time[-len(senkou_b):]
+    plt.fill_between(time_a[is_greater], green_a[is_greater], green_b[is_greater], color='g', alpha=0.1)
+    plt.fill_between(time_a[~is_greater], green_a[~is_greater], green_b[~is_greater], color='r', alpha=0.1)
+
+    axis.set_xlabel('time')
+    axis.set_title(f'{market} market')
+    # todo - rescale x-axis labels
+    plt.show()
+
+
+def get_period_highs(period, klines):
+    period_high = []
+    for i in range(period, len(klines)):
+        values_high = []
+        # todo- implement recursion here
+        for j in klines[i - period:i]:
+            values_high.append(j.open_price)
+        period_high.append(np.mean(values_high))
+    return np.array(period_high)
+
+
+def get_period_lows(period, klines):
+    period_low = []
+    for i in range(period, len(klines)):
+        values_low = []
+        # todo- implement recursion here
+        for j in klines[i - period:i]:
+            values_low.append(j.open_price)
+        period_low.append(np.mean(values_low))
+    return np.array(period_low)
+
+
+def get_chikou(period, klines):
+    values_chi = [i.close_price for i in klines[period:]]
+    return np.array(values_chi)
+
+
+def get_time(klines):
+    time_val = [i.open_t for i in klines]
+    return np.array(time_val)
