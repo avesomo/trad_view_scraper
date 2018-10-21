3377091477535be2509deabba3761e18f86aa523
avesomo <radoslaw.rygiel@gmail.com>
2018-10-21 20:03:14 +0200

    adding postgres files

---
+++ sql.py
@@ -0,0 +1,82 @@
+import psycopg2
+from config import config
+
+
+def db_exists(db_name):
+    try:
+        params = config()
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+        cur.execute(f"select exists(select * from information_schema.tables where table_name='{db_name}')")
+        return cur.fetchone()[0]
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+
+def create_db(db_name):
+    commands = [f"""
+        CREATE TABLE {db_name} (
+            id SERIAL PRIMARY KEY,
+            open_time TIMESTAMP NOT NULL,
+            open_price FLOAT NOT NULL,
+            high_price FLOAT NOT NULL,
+            low_price FLOAT NOT NULL,
+            close_price FLOAT NOT NULL,
+            volume FLOAT NOT NULL,
+            close_time TIMESTAMP NOT NULL,
+            quote FLOAT NOT NULL,
+            no_trades INTEGER NOT NULL,
+            taker_base FLOAT NOT NULL,
+            taker_quote FLOAT NOT NULL,
+            ignore_val INTEGER NOT NULL
+        )"""]
+    conn = None
+    try:
+        params = config()
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+        for command in commands:
+            cur.execute(command)
+        cur.close()
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+
+def create_markets_databases(markets, interval=('4H', )):
+    for ti in interval:
+        for market in markets:
+            db_name = f'{market}_{ti}'.lower()
+            if not db_exists(db_name):
+                create_db(db_name)
+
+
+def insert_klines(klines, db_name):
+    # todo - add only if such a kline doesnt exist - check the 'latest
+    # todo - row' and compare it to current_time
+    conn = None
+    transformed = [i.get_attributes() for i in klines]
+    try:
+        params = config()
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+        cur.execute(f"Select * FROM {db_name} LIMIT 0")
+        columns = ', '.join([desc[0] for desc in cur.description][1:])
+        print(f'Inserting klines data into sql table {db_name.upper()}...')
+        for kline in transformed:
+            sql = f"""INSERT INTO %s (%s) VALUES %s""" % (db_name, columns, kline)
+            cur.execute(sql)
+            conn.commit()
+        print(f'{db_name.upper()} table data has been updated.\n')
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
